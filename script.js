676571ecb0ece2b049e9adfaf6ee53557dc67192
moaaskt <moacirmayer@hotmail.com>
2022-04-13 17:34:33 -0300

    3 files modified

--- script.js
+++ script.js
@@ -1,47 +1,22 @@
-const numberButtons = document.querySelectorAll("[data-number]");
-const operationButtons = document.querySelectorAll("[data-operator]");
-const equalsButton = document.querySelector("[data-equals]");
-const deleteButton = document.querySelector("[data-delete]");
-const allClearButton = document.querySelector("[data-all-clear]");
-const previousOperandTextElement = document.querySelector("[data-previous-operand]");
-const currentOperandTextElement = document.querySelector("[data-current-operand]");
 
-class Calculator {
-  constructor(previousOperandTextElement,currentOperandTextElement){
-this.previousOperandTextElement = previousOperandTextElement;
-this.currentOperandTextElement = currentOperandTextElement;
-this.clear()
+function insert(num){
+  var numero = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = numero + num;
+}
+function clean(){
+  document.getElementById('resultado').innerHTML = "";
+}
+
+function backspace(){
+  var resultado = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function calcular(){
+  var resultado = document.getElementById('resultado').innerHTML;
+  if(resultado)
+  {
+      document.getElementById('resultado').innerHTML = eval(resultado)
   }
-
-// appendNumber(number){
-// this.currentOperand = `${this.currentOperand}${number.toString()}`;
-// }
-
-
-  clear(){
-    this.currentOperand = "";
-    this.previousOperand = "";
-    this.operation = undefined;
-  }
-
-updateDisplay(){
-  this.previousOperandTextElement.innerText = this.previousOperand;
-  this.currentOperandTextElement.innerText = this.currentOperand;
+  
 }
-
-}
-
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
-
-
-for (const numberButton of numberButtons){
-  numberButton.addEventListener("click", () => {
-    calculator.appendNumber(numberButton.innerText);
-    calculator.updateDisplay();
-  });
-}
-
-allClearButton.addEventListener("click", () => {
-  calculator.clear();
-  calculator.updateDisplay()
-});
